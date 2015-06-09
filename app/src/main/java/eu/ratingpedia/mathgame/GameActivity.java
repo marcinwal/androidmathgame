d1578869008608805d2e4fcded2b3a463cc2b8a8
marcinwal <marcin.walendzik@gmail.com>
2015-06-09 22:12:10 +0100

    linking varaibles with layout

--- app/src/main/java/eu/ratingpedia/mathgame/GameActivity.java
+++ app/src/main/java/eu/ratingpedia/mathgame/GameActivity.java
@@ -2,7 +2,8 @@ package eu.ratingpedia.mathgame;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class GameActivity extends Activity {
@@ -11,6 +12,31 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //init variables
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer - 2;
+
+        //linking to layout objects
+        TextView textObjectPartA = (TextView)findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
+
+        Button buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+        Button buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+        Button buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+
+        //setting the text fields by setText
+        textObjectPartA.setText(""+partA);
+        textObjectPartA.setText(""+partB);
+
+        buttonObjectChoice1.setText(""+correctAnswer);
+        buttonObjectChoice2.setText(""+wrongAnswer1);
+        buttonObjectChoice3.setText(""+wrongAnswer2);
+
+
     }
 
 
